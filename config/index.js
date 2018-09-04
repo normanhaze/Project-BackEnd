96064f6f7026200d563e079b9f46c73a55c8566c
normanhaze <h.normandale@gmail.com>
2018-09-04 14:48:38 +0100

    adding models, test data and seeing function

--- config/index.js
+++ config/index.js
@@ -3,6 +3,9 @@ const NODE_ENV = process.env.NODE_ENV || 'development';
 const config = {
     development: {
         DB_URL: 'mongodb://localhost:27017/project-backend'
+    },
+    test: {
+        DB_URL: 'mongodb://localhost:27017/project-backend-test'
     }
 };
 
